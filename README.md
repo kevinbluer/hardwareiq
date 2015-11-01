560c3aff33353d073a4a59c95bfe228ab79b1e36
Kevin Bluer <kevin@bluer.com>
2015-11-01 21:57:13 +0800

    updating icon and splash, adding readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Welcome to Hardware IQ
+
+### Increase your hardware intellect :)
+
+![](resources/icon.png)
--- www/js/services.js
+++ www/js/services.js
@@ -8,17 +8,20 @@ angular.module('starter.services', [])
     id: 0,
     name: 'Raspberry Pi',
     lastText: 'The Raspberry Pi is a low cost, credit-card sized computer',
-    face: 'img/raspberry-square.png'
+    face: 'img/raspberry-square.png',
+    url: 'https://www.raspberrypi.org'
   }, {
     id: 1,
     name: 'Arduino',
     lastText: 'Open-source electronic prototyping platform allowing to create interactive electronic objects.',
-    face: 'img/arduino.png'
+    face: 'img/arduino.png',
+    url: 'https://www.arduino.cc/'
   }, {
     id: 2,
     name: 'BeagleBone',
     lastText: 'BeagleBone is an $89 MSRP, credit-card-sized Linux computer that connects to the Internet and runs software such as Android 4.0 and Ubuntu.',
-    face: 'img/beagle-board.png'
+    face: 'img/beagle-board.png',
+    url: 'http://beagleboard.org/bone'
   }];
 
   return {
